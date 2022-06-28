1d0362d6621681f879a73e9bca212345ffc21f99
Maximilian Shaw <maximilianyoung52@gmail.com>
2022-06-28 13:55:10 -0400

    created models

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose") 
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, "Must be a valid email address!"]
+      },
+      thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'thought',
+    
+        }
+      ],
+      friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+        }
+      ],
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+    }
+  );
+
+  userSchema.virtual("friendCount").get(function(){
+    return this.friends.length
+  });
+  
+  const User = model('user', userSchema);
+
+module.exports = User;
